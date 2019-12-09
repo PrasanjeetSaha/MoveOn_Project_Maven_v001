ff240ebc8d7596629855ad8ae343da137ad46962
Prasanjeet <prasanjeetsaha@gmail.com>
2019-12-10 00:16:13 +0530

    Added try package.

---
+++ src/test/java/TryRun/Module1Objects.java
@@ -0,0 +1,10 @@
+package TryRun;
+
+import org.testng.annotations.Test;
+
+public class Module1Objects {
+	@Test
+	public void firstMethod(){
+		System.out.println("This is my object class");
+	}
+}
---
+++ src/test/java/TryRun/Module1Tests.java
@@ -0,0 +1,11 @@
+package TryRun;
+
+import org.testng.annotations.Test;
+
+public class Module1Tests {
+	@Test
+	public void secondMethod(){
+		System.out.println("This is my test class");
+	}
+
+}
