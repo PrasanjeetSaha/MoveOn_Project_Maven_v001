7e909bad61dbcbf2f6cdaff8106d3c36a8e18d16
Prasanjeet <prasanjeetsaha@gmail.com>
2019-12-12 08:13:34 +0530

    main method added.

--- src/test/java/TryRun/Module1Tests.java
+++ src/test/java/TryRun/Module1Tests.java
@@ -1,11 +1,32 @@
 package TryRun;
 
+import java.util.*;
+
 import org.testng.annotations.Test;
 
 public class Module1Tests {
 	@Test
 	public void secondMethod(){
 		System.out.println("This is my test class");
+	}
+	
+	public static void main(String args[]){
+		String s="1234 534 12";
+		ArrayList<Character> li = new ArrayList<Character>();
+		for(int i=0;i<s.length();i++){
+			if(s.charAt(i)!=' '){
+				li.add(s.charAt(i));
+			}
+		}
+		System.out.println(li);
+		TreeSet<Character> ts = new TreeSet<Character>(li);
+		int max = ts.last();
+		for(int j=ts.size(); j<li.size(); j++){
+			ts.add((char) (max+1));
+			max += 1;
+		}
+		
+		System.out.println(ts);		
 	}
 
 }
